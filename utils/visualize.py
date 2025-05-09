6ed6048b9868c5352a13dbf0916969b6eb44d7cc
Gaurang Tari <tarigaurang20@gmail.com>
2025-05-09 20:47:35 +0100

    Update visualize.py

--- utils/visualize.py
+++ utils/visualize.py
@@ -2,20 +2,25 @@ import matplotlib.pyplot as plt
 import torch
 
 def plot_prediction(inputs, masks, preds):
-    inputs = inputs.permute(0, 2, 3, 1).numpy()
-    masks = masks.squeeze(1).numpy()
-    preds = (preds.squeeze(1).numpy() > 0.5).astype(int)
+    inputs = inputs.cpu().permute(0, 2, 3, 1).detach().numpy()
+    masks = masks.cpu().squeeze(1).detach().numpy()
+    preds = torch.sigmoid(preds).cpu().squeeze(1).detach().numpy()
 
-    num_samples = min(3, len(inputs))
-    for i in range(num_samples):
-        fig, ax = plt.subplots(1, 3, figsize=(12, 4))
-        ax[0].imshow(inputs[i])
-        ax[0].set_title("Input Image")
-        ax[1].imshow(masks[i], cmap="gray")
-        ax[1].set_title("Ground Truth")
-        ax[2].imshow(preds[i], cmap="gray")
-        ax[2].set_title("Prediction")
-        for a in ax:
-            a.axis("off")
-        plt.tight_layout()
-        plt.show()
+    batch_size = inputs.shape[0]
+    fig, axs = plt.subplots(batch_size, 3, figsize=(12, 4 * batch_size))
+    if batch_size == 1:
+        axs = [axs]
+
+    for i in range(batch_size):
+        axs[i][0].imshow(inputs[i])
+        axs[i][0].set_title("Input")
+        axs[i][1].imshow(masks[i], cmap="gray")
+        axs[i][1].set_title("Ground Truth")
+        axs[i][2].imshow(preds[i], cmap="gray")
+        axs[i][2].set_title("Prediction")
+
+        for ax in axs[i]:
+            ax.axis("off")
+
+    plt.tight_layout()
+    plt.show()
