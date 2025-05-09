e7c77ab0711586fd6a85d28c5722008aca3da32b
Gaurang Tari <tarigaurang20@gmail.com>
2025-05-09 20:45:41 +0100

    Update unet.py

--- model/unet.py
+++ model/unet.py
@@ -10,33 +10,19 @@ class UNet(nn.Module):
                 nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
                 nn.ReLU(inplace=True),
                 nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
+                nn.ReLU(inplace=True)
             )
 
         self.encoder1 = conv_block(in_channels, 64)
         self.pool1 = nn.MaxPool2d(2)
-
         self.encoder2 = conv_block(64, 128)
         self.pool2 = nn.MaxPool2d(2)
 
-        self.encoder3 = conv_block(128, 256)
-        self.pool3 = nn.MaxPool2d(2)
+        self.bottleneck = conv_block(128, 256)
 
-        self.encoder4 = conv_block(256, 512)
-        self.pool4 = nn.MaxPool2d(2)
-
-        self.bottleneck = conv_block(512, 1024)
-
-        self.upconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.decoder4 = conv_block(1024, 512)
-
-        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.decoder3 = conv_block(512, 256)
-
-        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, 2, 2)
         self.decoder2 = conv_block(256, 128)
-
-        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, 2, 2)
         self.decoder1 = conv_block(128, 64)
 
         self.final = nn.Conv2d(64, out_channels, kernel_size=1)
@@ -44,20 +30,10 @@ class UNet(nn.Module):
     def forward(self, x):
         e1 = self.encoder1(x)
         e2 = self.encoder2(self.pool1(e1))
-        e3 = self.encoder3(self.pool2(e2))
-        e4 = self.encoder4(self.pool3(e3))
-        b = self.bottleneck(self.pool4(e4))
 
-        d4 = self.upconv4(b)
-        d4 = self.decoder4(torch.cat((e4, d4), dim=1))
+        b = self.bottleneck(self.pool2(e2))
 
-        d3 = self.upconv3(d4)
-        d3 = self.decoder3(torch.cat((e3, d3), dim=1))
+        d2 = self.decoder2(torch.cat([self.upconv2(b), e2], dim=1))
+        d1 = self.decoder1(torch.cat([self.upconv1(d2), e1], dim=1))
 
-        d2 = self.upconv2(d3)
-        d2 = self.decoder2(torch.cat((e2, d2), dim=1))
-
-        d1 = self.upconv1(d2)
-        d1 = self.decoder1(torch.cat((e1, d1), dim=1))
-
-        return torch.sigmoid(self.final(d1))
+        return self.final(d1)
