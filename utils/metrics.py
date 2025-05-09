585b384d3bc68952b3d1537bef523940f460ed31
Gaurang Tari <tarigaurang20@gmail.com>
2025-05-09 20:47:02 +0100

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,10 +1,9 @@
 import torch
 
-def dice_coefficient(pred, target, smooth=1e-6):
-    pred = pred.view(-1)
-    target = target.view(-1)
-    intersection = (pred * target).sum()
-    return (2. * intersection + smooth) / (pred.sum() + target.sum() + smooth)
+def dice_coefficient(preds, targets, threshold=0.5):
+    preds = (torch.sigmoid(preds) > threshold).float()
+    intersection = (preds * targets).sum()
+    return (2. * intersection) / (preds.sum() + targets.sum() + 1e-8)
 
 def iou_score(pred, target, smooth=1e-6):
     pred = pred.view(-1)
