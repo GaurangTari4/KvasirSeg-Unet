2aed7674f725eea1c1ffbe02d97b21cbf057114b
Gaurang Tari <tarigaurang20@gmail.com>
2025-05-09 20:45:03 +0100

    Update loader.py

--- dataset/loader.py
+++ dataset/loader.py
@@ -1,44 +1,27 @@
 import os
-import cv2
-import numpy as np
+from PIL import Image
 from torch.utils.data import Dataset
-import torch
+import torchvision.transforms as transforms
 
-def load_images_from_folder(folder, target_size=(256, 256), grayscale=False):
-    images = []
-    for filename in sorted(os.listdir(folder)):
-        img = cv2.imread(os.path.join(folder, filename), cv2.IMREAD_GRAYSCALE if grayscale else cv2.IMREAD_COLOR)
-        if img is not None:
-            img = cv2.resize(img, target_size)
-            if grayscale:
-                img = img / 255.0
-            images.append(img)
-    return np.array(images)
-
-def load_dataset(image_folder, mask_folder, target_size=(256, 256)):
-    images = load_images_from_folder(image_folder, target_size)
-    masks = load_images_from_folder(mask_folder, target_size, grayscale=True)
-    return images, masks
-
-class KvasirSegDataset(Dataset):
-    def __init__(self, images, masks, transform=None):
-        self.images = images
-        self.masks = masks
+class KvasirDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
         self.transform = transform
+        self.images = sorted(os.listdir(image_dir))
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        image = self.images[idx].astype(np.float32)
-        mask = self.masks[idx].astype(np.float32)
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx])
+
+        image = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")
 
         if self.transform:
-            augmented = self.transform(image=image, mask=mask)
-            image = augmented["image"]
-            mask = augmented["mask"].unsqueeze(0)
-        else:
-            image = torch.tensor(image).permute(2, 0, 1)
-            mask = torch.tensor(mask).unsqueeze(0)
+            image = self.transform(image)
+            mask = self.transform(mask)
 
         return image, mask
